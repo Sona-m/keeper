159508034bc5b799292ab7f8b2de9d2bbed2ab5d
Sonam <sonamkri1605@gmail.com>
2023-03-18 15:43:30 +0530

    Updates

---
+++ precache-manifest.8eff3a8c6517f6ea4d6ca29caaea4f7e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea3ed1443c7a0473973be02ec15721c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7b4b36cbf9bb4bf2c95",
+    "url": "/static/js/2.441cec6c.chunk.js"
+  },
+  {
+    "revision": "11f976468e407afbb6df",
+    "url": "/static/js/main.ea3994f8.chunk.js"
+  },
+  {
+    "revision": "641bb0516db3d047d70b",
+    "url": "/static/js/runtime-main.be65498d.js"
+  }
+]);
